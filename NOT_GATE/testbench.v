c9a048004717f20fa89b4fd8703420c496fa3c76
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-05 13:16:04 +0530

    Create testbench.v

---
+++ NOT_GATE/testbench.v
@@ -0,0 +1,10 @@
+module or_gate_tb;
+  reg a;
+  wire y;
+  not_gate a1(.a(a), .y(y)); 
+  initial begin
+    $monitor($time ,"a=%b,y=%b" ,a,y);
+           a=0;
+           #100 a=1;   
+  end 
+endmodule
