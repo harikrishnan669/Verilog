95ec70444e1d6771f2103e60eb704341b8ca6b9d
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-05 11:28:51 +0530

    Create testbench.v

---
+++ AND_GATE/testbench.v
@@ -0,0 +1,20 @@
+module and_gate_tb;
+  reg a, b;
+  wire y;
+  
+  and_gate a1(.a(a), .b(b), .y(y));
+  
+  initial begin
+    a = 1'b0; b = 1'b0;
+    #1 $display("a = %b b = %b y = %b", a, b, y);
+    
+    a = 1'b0; b = 1'b1;
+    #1 $display("a = %b b = %b y = %b", a, b, y);
+    
+    a = 1'b1; b = 1'b0;
+    #1 $display("a = %b b = %b y = %b", a, b, y);
+    
+    a = 1'b1; b = 1'b1;
+    #1 $display("a = %b b = %b y = %b", a, b, y);
+  end
+endmodule
