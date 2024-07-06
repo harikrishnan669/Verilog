cb014ee460053141a30428d7cdf78b4f809f82b9
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-06 22:19:52 +0530

    Create testbench.v

---
+++ NAND_GATE/testbench.v
@@ -0,0 +1,13 @@
+module nand_gate_tb;
+  reg a, b;
+  wire y;
+  nand_gate a1(.a(a), .b(b), .y(y));
+
+  initial begin
+    $monitor("a=%b, b=%b, y=%b", a, b, y);
+    a = 0; b = 0;
+    #100 a = 0; b = 1;
+    #100 a = 1; b = 0;
+    #100 a = 1; b = 1;
+  end
+endmodule
